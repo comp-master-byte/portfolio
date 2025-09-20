058de2c66d20b8a91381e4192385032d37df0b1e
comp-master-byte <demoniac781@gmail.com>
2025-09-20 17:02:06 +0300

    feat(header): added navigation bar

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,19 +1,18 @@
 import { memo } from "react";
 import styles from './Header.module.css';
-import { Button } from "@/shared/ui";
 
 export const Header = memo(function Header() {
   return (
     <header className={styles.header}>
-      <ul className={styles.navigationList}>
-        <li className={styles.navigationList__item}>Главная</li>
-        <li className={styles.navigationList__item}>Обо мне</li>
-        <li className={styles.navigationList__item}>Мои проекты</li>
-        <li className={styles.navigationList__item}>Менторство</li>
-        <li className={styles.navigationList__item}>Контакты</li>
-      </ul>
-
-      <Button>Связаться со мной</Button>
+      <div className={styles.headerGlassContainer}>
+        <ul className={styles.navigationList}>
+          <li className={styles.navigationList__item}>Главная</li>
+          <li className={styles.navigationList__item}>Обо мне</li>
+          <li className={styles.navigationList__item}>Мои проекты</li>
+          <li className={styles.navigationList__item}>Менторство</li>
+          <li className={styles.navigationList__item}>Контакты</li>
+        </ul>
+      </div>
     </header>
   )
 })
